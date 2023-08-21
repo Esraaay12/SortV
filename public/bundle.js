c79acb79b78549567af732a6c8b5cf835ada4f0b
Esraa Ayoub <58952187+Esraaay12@users.noreply.github.com>
2023-08-22 00:30:24 +0300

    Update bundle.js

--- public/bundle.js
+++ public/bundle.js
@@ -2,7 +2,7 @@
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
-/******/ 	// The require function
+/******/ 
 /******/ 	function __webpack_require__(moduleId) {
 /******/
 /******/ 		// Check if module is in cache
